d9ae3768af12f9e9da72a39783e1d2773e490aaf
unknown <poputa.alex@gmail.com>
2024-07-26 14:49:44 +0300

    Finished Task 9 & 10

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const btn = document.querySelector('.change-color');
+const text = document.querySelector('.color');
+
+const setGradient = () => {
+	body.style.background = `linear-gradient(to right, ${getRandomHexColor()}, ${getRandomHexColor()})`;
+	text.innerText = `${body.style.background};`;
+};
+
+btn.addEventListener('click', setGradient);
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,21 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('button[data-create]');
+const desytoryBtn = document.querySelector('button[data-destroy]');
+const container = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', () => {
+	for (let i = 0; i < input.value; i++) {
+		const box = `<div style="height:30px;width:30px;background:${getRandomHexColor()}"></div>`;
+		container.insertAdjacentHTML('beforeend', box);
+	}
+});
+
+desytoryBtn.addEventListener('click', () => {
+  container.replaceChildren();
+});
